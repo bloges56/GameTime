6d20c8ebb05691595669fd15aa0d4cd67691231a
bloges56 <brady.drew.logan@gmail.com>
2021-01-29 14:40:07 -0600

    added sql and created database

---
+++ SQL/01_Db_Create.sql
@@ -0,0 +1,84 @@
+﻿USE [master]
+
+IF db_id('GameTime') IS NULL
+  CREATE DATABASE [GameTime]
+GO
+
+USE [GameTime]
+GO
+
+
+DROP TABLE IF EXISTS [Friend];
+DROP TABLE IF EXISTS [UserSession];
+DROP TABLE IF EXISTS [Session];
+DROP TABLE IF EXISTS [User];
+GO
+
+
+CREATE TABLE [User] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [FirebaseUserId] NVARCHAR(28) NOT NULL,
+  [UserName] nvarchar(20) NOT NULL,
+  [Email] nvarchar(555) NOT NULL,
+  [Bio] nvarchar(1000) NOT NULL,
+  [IsActive] bit NOT NULL DEFAULT 1,
+
+  CONSTRAINT UQ_FirebaseUserId UNIQUE(FirebaseUserId),
+  CONSTRAINT UQ_Email UNIQUE(Email)
+)
+
+CREATE TABLE [Session] (
+	[Id] integer PRIMARY KEY IDENTITY,
+	[Title] nvarchar(50) NOT NULL,
+	[Time] datetime NOT NULL,
+	[Game] nvarchar(50) NOT NULL,
+	[OwnerId] integer NOT NULL,
+
+	CONSTRAINT [FK_Session_Owner] FOREIGN KEY ([OwnerId]) REFERENCES [User] ([Id])
+)
+
+CREATE TABLE [UserSession] (
+	[Id] integer PRIMARY KEY IDENTITY,
+	[SessionId] integer NOT NULL,
+	[UserId] integer NOT NULL,
+	[IsConfirmed] bit NOT NULL DEFAULT 0,
+
+	CONSTRAINT [FK_UserSession_User] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]),
+	CONSTRAINT [FK_UserSession_Session] FOREIGN KEY ([SessionId]) REFERENCES [Session] ([Id])
+)
+
+CREATE TABLE [Friend] (
+	[Id] integer PRIMARY KEY IDENTITY,
+	[UserId] integer NOT NULL,
+	[FriendId] integer NOT NULL,
+	[IsConfirmed] bit NOT NULL DEFAULT 0,
+
+	CONSTRAINT [FK_Friend_User] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]),
+	CONSTRAINT [FK_Friend_Friend] FOREIGN KEY ([FriendId]) REFERENCES [User] ([Id])
+)
+GO
+
+set identity_insert [User] on
+insert into [User] ([Id], [FirebaseUserId], [UserName], [Email], [Bio], [IsActive])
+values (1,'placeholder1', 'goldenGod', 'dennis@menace.com', 'Its about the implication', 1), 
+	(2, 'placeholder2', 'ratKing', 'bar@janitor.com', 'Oooh cat in the walls', 1),
+	(3, 'placeholder3', 'BigMac', 'ocular@patdown.com', 'Im building mass', 0),
+	(4, 'placeholder4', 'sweetDee', 'failed@actress.com', 'That stupid bitch', 1),
+	(5, 'placeholder5', 'mantisTobagan', 'filth@lover.com', 'Accidentally dropped my magnum condum for my massive dong', 1)
+set identity_insert [User] off
+
+set identity_insert [Session] on
+insert into [Session] ([Id], [Title], [Time], [Game], [OwnerId])
+values (1, 'Electric Boogaloo', '20210213 08:30:00 PM', 'CharDeeMacDennis', 1),
+	(2, 'ThunderGun Express', '20200129 09:00:00 PM', 'Monsters vs Aliens', 5)
+set identity_insert [Session] off
+
+set identity_insert [Friend] on
+insert into [Friend] ([Id], [UserId], [FriendId], [IsConfirmed])
+values (1, 1, 4, 1), (2, 4, 1, 1), (3, 2, 1, 0), (4, 1, 5, 1), (5, 5, 1, 1), (6, 5, 2, 1), (7, 2, 5, 1), (8, 1, 3, 1), (9, 3, 1, 1)
+set identity_insert [Friend] off
+
+set identity_insert [UserSession] on
+insert into [UserSession] ([Id], [SessionId], [UserId], [IsConfirmed])
+values (1, 1, 1, 1), (2, 1, 4, 0), (3, 1, 5, 1), (4, 2, 5, 1), (5, 2, 2, 1)
+set identity_insert [UserSession] off
